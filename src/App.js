66a0a8a526b154b9aef7c20cbf9bfc83041bb4e7
Jazmin Ivy <princessivy07@yahoo.com>
2023-04-06 12:05:31 -0500

    Installed Axios and started setting up the App Component to make axios requests to the API for all songs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect[() => {
+    getAllSongs();
+  }, []]
+
+  async function getAllSongs(){
+    let response = await axios.get();
+    setSongs(response.data);
+  }
+
   return (
     <div>
       <h3>Hello World!</h3>
