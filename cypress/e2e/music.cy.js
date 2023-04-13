8dbca6d73a900fa8b359fee4bb9f55aa48961627
Jazmin Ivy <princessivy07@yahoo.com>
2023-04-13 13:57:38 -0500

    Added Test 3, even though I don't have a delete function yet

--- cypress/e2e/music.cy.js
+++ cypress/e2e/music.cy.js
@@ -13,14 +13,15 @@ describe('Add Song form', () => {
     cy.contains("Song - Artist")
   })
 })
-    // cy.get('[data-ctest=title-input]').type('Title')
-    // cy.get('[data-cy=artist-input]').type('Artist')
-    // cy.get('[data-cy=album-input]').type('Album')
-    // cy.get('[data-cy=genre-input]').type('Genre')
-    // cy.get('[data-cy=release_date-input]').type('Release Date')
-    // cy.get('[data-cy=running_time-input]').type('Running Time')
-    // cy.get('[data-cy=submit-button]').click()
-    // cy.contains('type').click()
+//     // cy.get('[data-ctest=title-input]').type('Title')
+//     // cy.get('[data-cy=artist-input]').type('Artist')
+//     // cy.get('[data-cy=album-input]').type('Album')
+//     // cy.get('[data-cy=genre-input]').type('Genre')
+//     // cy.get('[data-cy=release_date-input]').type('Release Date')
+//     // cy.get('[data-cy=running_time-input]').type('Running Time')
+//     // cy.get('[data-cy=submit-button]').click()
+//     // cy.contains('type').click()
+
 describe("Filter/Search functionality", () => {
   before(() => {
     cy.visit()
@@ -30,6 +31,54 @@ describe("Filter/Search functionality", () => {
     cy.get("genre-input").type("Genre")
     cy.get("release-date-input").type("Release Date")
     cy.get("running-time-input").type("Running Time")
+    cy.get("submit-button").click()
   })
-  
+
+  it("should display only the matching songs when filtering by title", () => {
+    cy.get("search-input").type("Song")
+      cy.get("search-button").click()
+      cy.contains("Song - Artist")
+    })
+
+  it("should display only the matching songs when filtering by artist", () => {
+    cy.get("search-input").clear().type("Artist")
+    cy.get("search-button").click()
+    cy.contains("Song - Artist")
+  })
+
+  it("should display only the matching songs when filtering by album", () => {
+    cy.get("search-input").clear().type("Album")
+    cy.get("search-button").click()
+    cy.contains("Song - Artist")
+  })
+
+  it("should display only the matching songs when filtering by genre", () => {
+    cy.get("search-input").clear().type("Trap")
+    cy.get("search-button").click()
+    cy.contains("Song - Artist")
+  })
+
+  it("should display only the matching songs when filtering by release_date", () => {
+    cy.get("search-input").clear().type("Release_Date")
+    cy.get("search-button").click()
+    cy.contains("Song - Artist")
+  })
+
+  it("should display only the matching songs when filtering by running_time", () => {
+    cy.get("search-input").clear().type("Running_Time")
+    cy.get("search-button").click()
+    cy.contains("Song - Artist")
+  })
+})
+
+describe("Record deletion", () => {
+  before(() => {
+    cy.visit("songs")
+    cy.contains("Song - Artist").click()
+  })
+
+  it("should delete the selected song", () => {
+    cy.get("delete-button").click()
+    cy.contains("Song - Artist").should("not.exist")
+  })
 })
